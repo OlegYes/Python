b8f0d0209cf4dcbff4e39b18d7ac4279589fa6f6
OlegYes <99252117+OlegYes@users.noreply.github.com>
2022-02-10 13:08:29 +0200

    Python progres step 1

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-print(3 + 4)
-print(3 * 5)
-print(3 ** 2)
-name = input("Как Вас зовут? ")
-print("Привет,", name)
+a = int(input())
+if a < -5:
+    print('Low')
+elif -5 <= a <= 5:
+    print('Mid')
+else:
+    print('High')
