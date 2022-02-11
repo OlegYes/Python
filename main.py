819c3f2e597e1300d27df80d435b8177b7bef5eb
OlegYes <99252117+OlegYes@users.noreply.github.com>
2022-02-11 11:29:32 +0200

    робота з даними через файл

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-print(3 + 4)
-print(3 * 5)
-print(3 ** 2)
-name = input("Как Вас зовут? ")
-print("Привет,", name)
+f = open('print_text.py', 'r')
+
+print(eval(f.readlines()))
+f.close()
+
+# with open('print_text.py', 'r') as f:
+#     lines = f.readlines()
+#     commands = ""
+#     for line in lines:
+#         commands = commands + line.replace("\n", "; ")
+#     print(commands)
+#     eval(commands)
